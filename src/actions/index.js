2078d5773f8027292590b707a6e1adae2bd2bf05
Jonathan Torres <Jonathan.torres25@live.com>
2018-04-09 14:35:45 -0400

    http to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY ='cf747a96f045d3b7a5879cccaa129afd';
-const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL =`https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
